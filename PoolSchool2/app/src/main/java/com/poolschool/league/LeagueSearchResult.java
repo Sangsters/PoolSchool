fb4a75bf72793f3bcdc80f7e58216af5d84109e7
s1pham <s1pham@yahoo.com>
2015-05-29 15:07:45 -0700

    pics for fundamental

--- PoolSchool2/app/src/main/java/com/poolschool/league/LeagueSearchResult.java
+++ PoolSchool2/app/src/main/java/com/poolschool/league/LeagueSearchResult.java
@@ -4,27 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.poolschool.poolschool.R;
 
-import java.io.BufferedReader;
+
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
+
 
 public class LeagueSearchResult extends Activity {
     //private FileInputStream fis;
-    //private String fileName;
-    // private String [] teamName;
+   //private String fileName;
+    //private String[] teamName;
 
 
-     @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-         setContentView(R.layout.league_search_result);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.league_search_result);
 
-         // Bundle extras = getIntent().getExtras();
-         //if(extras != null){
-         //teamName = extras.getStringArray("key");
-         //}
+        //Bundle extras = getIntent().getExtras();
+       // if (extras != null) {
+           // teamName = extras.getStringArray("key");
+       //}
 
-         // }
-     }
-
+    }
 }
+
+
